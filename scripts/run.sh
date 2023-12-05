fff8bd6f3cb0ac17de23569ad0472176846a9799
reda-bouzad <redabouzadfst@gmail.com>
2023-12-05 23:47:41 +0100

    devops

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,12 +5,9 @@ DIR=$(dirname "$(realpath "$0")")
 cd "$DIR"
 
 export DEBIAN_FRONTEND=noninteractive
-<<<<<<< HEAD
-=======
 
 APP_PORT=$(cat $DIR/port.txt)
 
 # Run the app
 cd "$DIR/../src"
 uvicorn main:app --host 0.0.0.0 --port $APP_PORT
->>>>>>> develop
